1f57ce07a56b2674a4688e05dec7491b4409a1e5
Davyd Krykota <krikotadavid957@gmail.com>
2025-03-04 13:11:16 +0200

    added class manager for money managing

---
+++ expanses.js
@@ -0,0 +1,19 @@
+export class manager {
+    constructor(balance = 0, currency = "UAN" ) {
+        this.balance = balance;
+        this.transactions = [];
+        this.currency = currency;
+    }
+    addIncome() {
+
+    }
+    addExpenses() {
+
+    }
+    getTransaction() {
+
+    }
+    getBalance() {
+
+    }
+}
